6609c348dc3008cdae9684d87aad004bacb434ce
Blair Myhre <120442257+cocosbeans@users.noreply.github.com>
2024-09-04 11:25:31 -0500

    Create fibonacci.cs

---
+++ fibonacci.cs
@@ -0,0 +1,12 @@
+namespace Fibonacci;
+
+class Program {
+  static int Fibonacci(int n) {
+    if (n <= 1) return n;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+  }
+  
+  static void Main() {
+    Fibonacci(5);
+  }
+}
